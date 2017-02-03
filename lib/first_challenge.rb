ea539f86782f3e4d7f87da8dc4c7c03d4f390b02
David Mieloch <davidmieloch@gmail.com>
2017-02-03 23:27:44 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,66 @@
 def first_challenge
-   epic_tragedy = {}
+  epic_tragedy = {
+    :montague => {},
+    :capulet => {}
+  }
 end
+
+# def first_challenge
+#   epic_tragedy = [
+#
+#     :montague => {
+#       :patriarch => {
+#         :name => "Lord Montague",
+#         :age => "53"
+#       },
+#       :matriarch => {
+#         :name => "Lady Montague",
+#         :age => "54"
+#       },
+#       :hero => {
+#         :name => "Romeo",
+#         :age => "15",
+#         :status => "Alive"
+#       },
+#       :hero_friends => [
+#         :friend_1 => {
+#           :name => "Benvolio",
+#           :age => "17",
+#           :attitude => "worried"
+#         },
+#         :friend_2 => {
+#           :name => "Benvolio",
+#           :age => "30",
+#           :attitude => "confused"
+#         },
+#       ],
+#     },
+#
+#     :capulet => {
+#       :patriarch => {
+#         :name => "Lord Capulet",
+#         :age => "50"
+#       },
+#       :matriarch => {
+#         :name => "Lady Capulet",
+#         :age => "52"
+#       },
+#       :heroine => {
+#         :name => "Lord Montague",
+#         :age => "53"
+#       },
+#       :heroine_friends => [
+#         :friend_1 => {
+#           :name => "Mercutio",
+#           :age => "18",
+#           :attitude => "hot-headed"
+#         },
+#         :friend_2 => {
+#           :name => "Nurse",
+#           :age => "44",
+#           :attitude => "worried"
+#         },
+#       ],
+#     },
+#   ]
+# end
